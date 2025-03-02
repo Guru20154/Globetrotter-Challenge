594b1de022d65d1339713e923e0093c80dde19bd
Guru20154 <gurkanwal04singh@gmail.com>
2025-03-02 11:26:51 +0530

    cors

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://globetrotter-challenge-delta.vercel.app/",
   credentials: true,     
 }))
 app.use(express.json());
